519a187dcdba0a8592afaba44e7bee1cd8cc368d
davidodidi <davidodidi@yahoo.co.uk>
2022-05-14 05:16:11 -0400

    Add files via upload

---
+++ email-scheduler/src/main/java/com/example/email/scheduler/web/EmailSchedulerController.java
@@ -0,0 +1,94 @@
+package com.example.email.scheduler.web;
+
+import com.example.email.scheduler.payload.EmailRequest;
+import com.example.email.scheduler.payload.EmailResponse;
+import com.example.email.scheduler.quartz.job.EmailJob;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class EmailSchedulerController {
+
+    @Autowired
+    private Scheduler scheduler;
+
+    @RequestMapping("/")
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }
+
+
+
+
+    @PostMapping("/schedule/email")
+    public ResponseEntity<EmailResponse> scheduleEmail(@Valid @RequestBody EmailRequest emailRequest){
+        try {
+            ZonedDateTime dateTime = ZonedDateTime.of(emailRequest.getDateTime(), emailRequest.getTimeZone());
+            if(dateTime.isBefore(ZonedDateTime.now())){
+               EmailResponse emailResponse = new EmailResponse(false,
+                       "dateTime must be after current time ");
+               return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(emailResponse);
+            }
+
+            JobDetail jobDetail = buildJobDetail(emailRequest);
+            Trigger trigger = buildTrigger(jobDetail, dateTime);
+
+            scheduler.scheduleJob(jobDetail, trigger);
+
+            EmailResponse emailResponse = new EmailResponse(true,
+                    jobDetail.getKey().getName(), jobDetail.getKey().getGroup(),
+                    "Email Scheduled Successfully!");
+
+            return ResponseEntity.ok(emailResponse);
+
+        } catch (SchedulerException se){
+            log.error("Error while scheduling email: ", se);
+            EmailResponse emailResponse = new EmailResponse(false,
+                    "Error while scheduling email. Please try again later");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(emailResponse);
+        }
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<String> getApiTest(){
+        return ResponseEntity.ok("Get API Test - Pass");
+    }
+
+    private JobDetail buildJobDetail(EmailRequest scheduleEmailRequest){
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put("email", scheduleEmailRequest.getEmail());
+        jobDataMap.put("subject", scheduleEmailRequest.getSubject());
+        jobDataMap.put("body", scheduleEmailRequest.getBody());
+
+        return JobBuilder.newJob(EmailJob.class)
+                .withIdentity(UUID.randomUUID().toString(), "email-jobs")
+                 .withDescription("Send Email Job")
+                .usingJobData(jobDataMap)
+                .storeDurably()
+                .build();
+    }
+
+    private Trigger buildTrigger(JobDetail jobDetail, ZonedDateTime startAt){
+        return TriggerBuilder.newTrigger()
+                .forJob(jobDetail)
+                .withIdentity(jobDetail.getKey().getName(), "email-triggers")
+                .startAt(Date.from(startAt.toInstant()))
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withMisfireHandlingInstructionFireNow())
+                .build();
+    }
+}
